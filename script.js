a944c200d60bb31131d24abb2a030a3707c7c672
Jakezki <145100021+Jakezki@users.noreply.github.com>
2024-01-30 13:07:41 +0200

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-const rsvpForm = document.getElementById('rsvp-form');
-const confirmationMessage = document.getElementById('confirmation-message');
-const body = document.body;
-rsvpForm.addEventListener('submit', (event) => {
-  event.preventDefault();
+const ilmoittautumislomake = document.getElementById('rsvp-form');
+const vahvistusviesti = document.getElementById('confirmation-message');
+const ruumis = document.body;
+ilmoittautumislomake.addEventListener('submit', (tapahtuma) => {
+  tapahtuma.preventDefault();
 
-  const attendance = document.getElementById('attendance').value;
+  const osallistuminen = document.getElementById('attendance').value;
 
-  if (attendance === 'yes') {
-    confirmationMessage.innerHTML = '🎉 Party on! We look forward to seeing you at the GIF Gala!';
-    body.style.backgroundImage = 'url("https://media.giphy.com/media/l2JHPB58MjfV8W3K0/giphy.gif")';
-  } else if (attendance === 'no') {
-    confirmationMessage.innerHTML = '😔 We will miss you at the GIF Gala!';
-    body.style.backgroundImage = 'url("https://media.giphy.com/media/JER2en0ZRiGUE/giphy.gif")';
+  if (osallistuminen === 'yes') {
+    vahvistusviesti.innerHTML = '🎉 Juhlat jatkuvat! Odotamme innolla näkevämme sinut GIF Gaalassa!';
+    ruumis.style.backgroundImage = 'url("https://media.giphy.com/media/l2JHPB58MjfV8W3K0/giphy.gif")';
+  } else if (osallistuminen === 'no') {
+    vahvistusviesti.innerHTML = '😔 Kaipaamme sinua GIF Gaalassa!';
+    ruumis.style.backgroundImage = 'url("https://media.giphy.com/media/JER2en0ZRiGUE/giphy.gif")';
   }
 
-  confirmationMessage.style.display = 'block';
+  vahvistusviesti.style.display = 'block';
 
-  rsvpForm.reset();
+  ilmoittautumislomake.reset();
 });
+
